18521997689c360ed6f117d6e32dc08bb378df0f
Xian Li <xnslx89@gmail.com>
2020-05-06 23:12:18 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, {useState,useReducer} from 'react';
 import './App.css';
 import SumUp from './Component/SumUp/SumUp';
 import Item from './Component/Item/Item';
@@ -24,43 +24,44 @@ const reducer = (state,action) => {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state1, dispatch1] = useReducer(reducer, initialState);
+  const [state2, dispatch2] = useReducer(reducer, initialState);
+  const [state3, dispatch3] = useReducer(reducer, initialState);
+  const [count, setCount] = useState(initialState);
+  const [itemNum, setItemNum] = useState(0)
 
-  // const incHandler = (e) => {
-    
-  // };
+  const changeHandler = e => {
+    setCount({count:e.target.value})
+    setItemNum({itemNum:1})
+  }
 
-  // const decHandler = (e) => {
-    
-  // };
-
-  // const delateHandler = (e) => {
-    
-  // };
+  const itemNumHandler = e => {
+    setItemNum({itemNum:1})
+  }
   return (
     <div>
-      <SumUp value={state.count}/>
+      <SumUp changed={itemNumHandler} value={itemNum}/>
       <div style={{marginTop: '40px', marginLeft: '40px'}}>
         <Item
-          value={state.count}
-          incHandler={() => dispatch({type: 'INCREMENT'})} 
-          decHandler={() => dispatch({type: 'DECREMENT'})} 
-          delateHandler={() => dispatch({type: 'DELATE'})}
+          value={state1.count}
+          changed={changeHandler}
+          incHandler={() => dispatch1({type: 'INCREMENT'})} 
+          decHandler={() => dispatch1({type: 'DECREMENT'})} 
+          delateHandler={() => dispatch1({type: 'DELATE'})}
         />
         <Item
-          incHandler={() => dispatch({type: 'INCREMENT'})} 
-          decHandler={() => dispatch({type: 'DECREMENT'})} 
-          delateHandler={() => dispatch({type: 'DELATE'})}
+          value={state2.count}
+          changed={changeHandler}
+          incHandler={() => dispatch2({type: 'INCREMENT'})} 
+          decHandler={() => dispatch2({type: 'DECREMENT'})} 
+          delateHandler={() => dispatch2({type: 'DELATE'})}
         />
         <Item
-          incHandler={() => dispatch({type: 'INCREMENT'})} 
-          decHandler={() => dispatch({type: 'DECREMENT'})} 
-          delateHandler={() => dispatch({type: 'DELATE'})}
-        />
-        <Item
-          incHandler={() => dispatch({type: 'INCREMENT'})} 
-          decHandler={() => dispatch({type: 'DECREMENT'})} 
-          delateHandler={() => dispatch({type: 'DELATE'})}
+          value={state3.count}
+          changed={changeHandler}
+          incHandler={() => dispatch3({type: 'INCREMENT'})} 
+          decHandler={() => dispatch3({type: 'DECREMENT'})} 
+          delateHandler={() => dispatch3({type: 'DELATE'})}
         />
       </div>
     </div>
--- src/Component/SumUp/SumUp.js
+++ src/Component/SumUp/SumUp.js
@@ -3,11 +3,14 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const SumUp = (props) => {
     return (
-        <div Style={{width:'100%', height: '80px',backgroundColor: '#999999'}}>
+        <div style={{width:'100%', height: '80px',backgroundColor: '#f0f0f0'}}>
             <FontAwesomeIcon icon="shopping-cart" style={{fontSize: '28px'}}/>
-            <span style={{marginLeft: '20px'}}>
-                {props.value}items
-            </span>
+            <input 
+                style={{marginLeft: '20px'}} 
+                type="number"
+                onChange={props.changed}
+                value={props.value}
+            />items
         </div>
     )
 };
