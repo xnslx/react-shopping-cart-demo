7da814aad3a4dda87e379429ce234dc2d7310d6a
Xian Li <xnslx89@gmail.com>
2020-05-08 14:51:33 -0500

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,68 +1,61 @@
-import React, {useState,useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
-import SumUp from './Component/SumUp/SumUp';
-import Item from './Component/Item/Item';
+import Items from '../src/Component/Items/Items';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faShoppingCart} from '@fortawesome/free-solid-svg-icons';
 library.add(faShoppingCart);
 
-const initialState = {
-  count: 0
+const initialState = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+  { id: 3, value: 0 },
+  { id: 4, value: 0 }
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state.map(item => {
+        if (item.id === action.id) {
+          return {...item, value: item.value +1}
+        }
+        return item;
+      })
+    case "DECREMENT":
+        return state.map(item => {
+          if (item.id === action.id) {
+            return {...item, value: item.value - 1}
+          }
+          return item;
+        })
+    case "DELATE":
+      return state.filter(item => item.id !== action.id)
+    default:
+      return state;
+  }
 };
 
-const reducer = (state,action) => {
-  switch(action.type) {
-    case 'INCREMENT': 
-      return {count: state.count + 1};
-    case 'DECREMENT' :
-      return { count: state.count - 1};
-    case 'DELATE':
-      return initialState;
-    default:
-      return state
-  }
-}
-
 function App() {
-  const [state1, dispatch1] = useReducer(reducer, initialState);
-  const [state2, dispatch2] = useReducer(reducer, initialState);
-  const [state3, dispatch3] = useReducer(reducer, initialState);
-  const [count, setCount] = useState(initialState);
-  const [itemNum, setItemNum] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [setItems] = useState(initialState)
 
   const changeHandler = e => {
-    setCount({count:e.target.value})
-    setItemNum({itemNum:1})
-  }
-
-  const itemNumHandler = e => {
-    setItemNum({itemNum:1})
+    setItems({items: e.target.value})
   }
   return (
-    <div>
-      <SumUp changed={itemNumHandler} value={itemNum}/>
-      <div style={{marginTop: '40px', marginLeft: '40px'}}>
-        <Item
-          value={state1.count}
-          changed={changeHandler}
-          incHandler={() => dispatch1({type: 'INCREMENT'})} 
-          decHandler={() => dispatch1({type: 'DECREMENT'})} 
-          delateHandler={() => dispatch1({type: 'DELATE'})}
-        />
-        <Item
-          value={state2.count}
-          changed={changeHandler}
-          incHandler={() => dispatch2({type: 'INCREMENT'})} 
-          decHandler={() => dispatch2({type: 'DECREMENT'})} 
-          delateHandler={() => dispatch2({type: 'DELATE'})}
-        />
-        <Item
-          value={state3.count}
-          changed={changeHandler}
-          incHandler={() => dispatch3({type: 'INCREMENT'})} 
-          decHandler={() => dispatch3({type: 'DECREMENT'})} 
-          delateHandler={() => dispatch3({type: 'DELATE'})}
-        />
+    <div className="App">
+      <h1>The total items: </h1>
+      <div>
+        {state.map(item => (
+          <Items
+            value={item.value}
+            key={item.id}
+            changed={changeHandler}
+            incHandler={() => dispatch({ type: "INCREMENT", id: item.id })}
+            decHandler={() => dispatch({ type: "DECREMENT", id:item.id })}
+            delateHandler={() => dispatch({ type: "DELATE", id:item.id })}
+          />
+        ))}
       </div>
     </div>
   );
--- src/Component/Items/Items.js
+++ src/Component/Items/Items.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
-const Item = (props) => {
+const Items = (props) => {
+    // console.log(props)
     return (
         <div>
-            <input 
-                type="text" 
-                style={{width: '120px', height: '40px'}}
-                value={props.value}
-                onChange={props.changed}
-            />
+            <input type="number" value={props.value} onChange={props.changed}/>
             <button onClick={props.incHandler}>+</button>
             <button onClick={props.decHandler}>-</button>
             <button onClick={props.delateHandler}>X</button>
         </div>
     )
-};
+}
 
-export default Item;
+export default Items;
